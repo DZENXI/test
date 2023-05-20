107f2cfd21dfea03f7e53660c9f3af5543037199
Valentayne <129650007+Valentayne@users.noreply.github.com>
2023-05-20 12:54:33 +0300

    Що я наробивgit status

---
+++ StructMyFrac.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    struct MyFrac
+    {
+        public long nom, denom;
+        public MyFrac(long nom_, long denom_)
+        {
+            nom = 0; denom = 0;
+            long firstnom = nom_, secdenom = denom_;
+            if (denom_ < 0)
+            {
+                firstnom = -nom_; secdenom = -denom_; nom_ = -nom_; denom_ = -denom_;
+            }
+            while ((firstnom != 0) && (secdenom != 0))
+            {
+                if (firstnom > secdenom)
+                {
+                    firstnom = firstnom % secdenom;
+                }
+                else
+                {
+                    secdenom = firstnom % secdenom;
+                }
+            }
+            long sum = firstnom + secdenom;
+            if (sum > 0)
+            {
+                nom = nom_ / sum; denom = denom_ / sum;
+            }
+            else
+            {
+                nom = nom_; denom = denom_;
+            }
+
+        }
+        public override string ToString()
+        {
+            return $"{nom}/{denom}";
+        }
+    }
+}
